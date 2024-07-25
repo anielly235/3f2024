bfd4142a5eb7f3754caf8fae8cc87b30a100d700
anielly235 <110236221+anielly235@users.noreply.github.com>
2024-07-25 09:42:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,41 +14,25 @@ function calculaTempo(tempoObjetivo){
     let minutos
     let horas
     let dias
+    segundos = (tempoObjetivo - agora)/1000
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos)
+    minutos = Math.floor(minutos)
+    horas = Math.floor(horas)
+    dias = Math.floor(dias)
 
-    segundos = (tempoObjetivo - agora)/1000;
-    minutos = segundos/60;
-    horas = minutos/60;
-    dias = horas/24;
-
-    segundos = Math.floor(segundos);
-    minutos = Math.floor(minutos);
-    horas = Math.floor(horas);
-    dias = Math.floor(dias);
-
-    segundos = segundos%60;
-    minutos = minutos%60;
-    horas = horas%24;
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
     return [dias,horas,minutos,segundos]
-
 }
 let contadorPrimeiro = calculaTempo(tempoObjetivo1)
-contador[0].textContent = `faltam ${contadorPrimeiro[0]} dias, ${contadorPrimeiro[1]} horas, ${contadorPrimeiro[2]} minutos,
+contador[0].textContent = `Faltam ${contadorPrimeiro[0]} dias, ${contadorPrimeiro[1]} horas, ${contadorPrimeiro[2]} minutos e ${contadorPrimeiro[3]} segundos`
 let contadorSegundo = calculaTempo(tempoObjetivo2)
-contador[1].textContent = `faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas, ${contadorSegundo[2]} minutos, ${contadorSegundo[3]} segundos`
-calculaTempo(tempoObjetivo1);
-calculaTempo(tempoObjetivo2);
-calculaTempo(tempoObjetivo3);
-calculaTempo(tempoObjetivo4);
-
-
-
-for (let i = 0 ; i < botoes.length ; i++){
-    botoes[i].onclick = function (){
-        for (let j = 0; j < botoes.length ; j++){
-        botoes[j].classList.remove("ativo");
-        conteudo[j].classList.remove('ativo')
-        }
-        botoes[i].classList.add("ativo");
-        conteudo[i].classList.add("ativo")
-    }
-}
+contador[1].textContent = `Faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas e ${contadorSegundo[2]} minutos e ${contadorSegundo[3]} segundos`
+let contadorTerceiro = calculaTempo(tempoObjetivo2)
+contador[2].textContent = `Faltam ${contadorTerceiro[0]} dias, ${contadorTerceiro[1]} horas e ${contadorTerceiro[2]} minutos e ${contadorTerceiro[3]} segundos`
+let contadorQuarto = calculaTempo(tempoObjetivo2)
+contador[3].textContent = `Faltam ${contadorQuarto[0]} dias, ${contadorQuarto[1]} horas e ${contadorQuarto[2]} minutos e ${contadorQuarto[3]} segundos`
